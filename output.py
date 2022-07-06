bc1865a51599e4942f87ef23b10c05ea46ead570
clintonatspace <76195382+clintonatspace@users.noreply.github.com>
2022-07-07 02:21:24 +0530

    Update output.py

--- output.py
+++ output.py
@@ -4,4 +4,18 @@ import injector
 import math
 import scipy
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import burnrate as br
+
+
+
+
+xpoints = br.burn_time_array
+ypoints = incermental_analysis.mass_flux
+plt.plot(xpoints, ypoints)
+plt.show()
+
+xpoints = br.burn_time_array
+ypoints = incermental_analysis.vol_array
+plt.plot(xpoints, ypoints)
+plt.show()
